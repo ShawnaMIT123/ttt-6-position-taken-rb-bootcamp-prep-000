46fe9c7c04a5b101ba97ba60ce27dee2538e7fbf
Shawna Davis <shawna_davis@alum.mit.edu>
2018-06-30 20:53:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
- if array[index] == " "
+ if array[index] == " " || array[index] == "" ||array[index] == NIL
    return true
   else
     return FALSE
